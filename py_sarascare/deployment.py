2ab99dac2a9f48da561d7fe56513e2df82734429
sanduni-sc <160084198+sanduni-sc@users.noreply.github.com>
2024-03-07 10:05:29 +1100

    Update deployment.py

--- py_sarascare/deployment.py
+++ py_sarascare/deployment.py
@@ -30,11 +30,8 @@ connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
 
 DATABASES = {
-	default: {
-		'ENGINE': 'django.db.backends.postgresql'
-		'NAME': parameters['dbname']
-		'HOST': parameters['host']
-		'USER': parameters['user']
-		'PASSWORD': parameters['password']
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
