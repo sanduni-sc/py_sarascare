529b970d97ca875f81edb71010cc246b72a2e798
sanduni-sc <160084198+sanduni-sc@users.noreply.github.com>
2024-03-07 11:19:46 +1100

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management import execute_from_command_line
 
 def main():
     """Run administrative tasks."""
@@ -17,6 +17,9 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+if __name__ == '__main__':
+    # Use the PORT environment variable or default to 8000
+    port = int(os.environ.get('PORT', 8000))
 
-if __name__ == "__main__":
-    main()
+    # Start the development server
+    execute_from_command_line(['manage.py', 'runserver', f'0.0.0.0:{port}'])
